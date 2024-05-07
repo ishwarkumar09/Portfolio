536471044fd548f9b9972b5eb0482f0b9be29ce1
ishwarkumar09 <ishwarborana4600@gmail.com>
2024-05-08 01:17:02 +0530

    React App setUp Completed

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,7 +4,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-     <h1>Mern Portfolio</h1>
+     <h1 className='text-3xl font-bold underline'>Mern Portfolio</h1>
     </div>
   );
 }
