586cea18418e7f15ab6b0bc13f83a46a6fda11d0
ishwarkumar09 <ishwarborana4600@gmail.com>
2024-05-20 16:47:14 +0530

    path error fixing

--- backend/server.js
+++ backend/server.js
@@ -14,13 +14,12 @@ app.use(express.json());
 app.use("/api/portfolio", dataRoutes)
 app.use("/api/portfolio/admin", adminRoutes)
 
-if (process.env.NODE_ENV === 'production') {
-    app.use(express.static(path.join(__dirname, 'frontend', 'build')));
-  
-    app.get('*', (req, res) => {
-      res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
-    });
-  }
+app.use(express.static(path.join(__dirname ,"/frontend/dist")))
+
+app.get("" ,(req,res)=>{
+  res.sendFile(path.join(__dirname ,"frontend" ,"dist" ,"index.html"))
+})
+
 
 connectDB()
 .then(()=>{
